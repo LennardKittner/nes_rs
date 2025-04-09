20559fbcf1ca9c89cdcc1511a0728e0f260f197a
Lennard Kittner <lennardkittner@icloud.com>
2025-04-09 11:02:27 +0200

    Fix test

--- tests/branch_timing_tests.rs
+++ tests/branch_timing_tests.rs
@@ -6,7 +6,7 @@ use std::sync::{Arc, Mutex};
 
 fn test_rom(path: &str) {
     let rom = Rom::load_from_disk(path).unwrap();
-    let mirroring = rom.screen_mirroring;
+    let mirroring = rom.get_mirroring_mode();
 
     let tile1 = Arc::new(Mutex::new(0));
 
@@ -16,14 +16,14 @@ fn test_rom(path: &str) {
         0f64,
         |ppu, _, _| {
             let (main_name_table, _) = match (mirroring, ppu.address_register.get_name_table()) {
-                (Mirroring::VERTICAL, 0b00)
-                | (Mirroring::VERTICAL, 0b10)
-                | (Mirroring::HORIZONTAL, 0b00)
-                | (Mirroring::HORIZONTAL, 0b01) => (&ppu.vram[0..0x400], &ppu.vram[0x400..0x800]),
-                (Mirroring::VERTICAL, 0b01)
-                | (Mirroring::VERTICAL, 0b11)
-                | (Mirroring::HORIZONTAL, 0b10)
-                | (Mirroring::HORIZONTAL, 0b11) => (&ppu.vram[0x400..0x800], &ppu.vram[0..0x400]),
+                (Mirroring::Vertical, 0b00)
+                | (Mirroring::Vertical, 0b10)
+                | (Mirroring::Horizontal, 0b00)
+                | (Mirroring::Horizontal, 0b01) => (&ppu.vram[0..0x400], &ppu.vram[0x400..0x800]),
+                (Mirroring::Vertical, 0b01)
+                | (Mirroring::Vertical, 0b11)
+                | (Mirroring::Horizontal, 0b10)
+                | (Mirroring::Horizontal, 0b11) => (&ppu.vram[0x400..0x800], &ppu.vram[0..0x400]),
                 (_, _) => panic!("Unsupported mirroring mode: {:?}", mirroring),
             };
 
